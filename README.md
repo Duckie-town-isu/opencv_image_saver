7dd506b5e9222de0834fb3455107ff0b43afdfb4
Ranai <ranais@iastate.edu>
2023-09-27 14:40:02 -0500

    Adding all files

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-## opencv_image_saver
+# opencv_image_saver
 
+### How to build
 1. Copy this file to your catkin_ws dir
 2. run `catkin_make`
 3. source catkin_ws/devel/setup.bash
 4. Run `roslaunch opencv_image_saver image_saver.launch`
 
-To save an image, hit space and then enter
+### How to use
+
+The node constructor ImageSaver has 3 parameters
+- `robot_name`: The name of the robot. If this is wrong, no image will be subscribed. _By default duck7_
+- `use_for_data-gathering`: Enables a different file naming scheme._By default True_. 
+- `store_location`: The place where to store all the images. _By default this value is <~userhome>/duckietown\_dataset_
+
+**In the python file, there is an option to use this for a dataset, by default this value is True** 
+- In this mode, enter 3 numbers in this format `[NUM_DUCKS] [NUM_ROBOTS] [NUM_CONES]`. The spaces are _VERY IMPORTANT_
+- To save an image, hit space and then enter
+
+This will save the image in the specified folder With the SUffixes DK for number of Ducks, RB for number of robots and CN for number of cones
--- src/image_saver.py
+++ src/image_saver.py
@@ -15,15 +15,19 @@ from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
 
 class ImageSaver():
-    def __init__(self, store_location=f"{Path.home()}/duckietown_dataset", robot_name="duck7") -> None:
+    def __init__(self, use_for_data_gathering=True, store_location=f"{Path.home()}/duckietown_dataset", robot_name="duck7") -> None:
         # get image from ROS
         self.img_subscriber = rospy.Subscriber(f"/{robot_name}/camera_node/image/compressed", CompressedImage, self.image_handler)
         
         self.save_dir_path = store_location
         self.create_save_dir(store_location)
         self.image_counter = 0
+        self.use_for_dataset = use_for_data_gathering
         self.bridge = CvBridge()
         self.save_flag = False
+        print("Initialization complete")
+        print("To save an image, enter 3 numbers with spaces in between and press enter")
+        print("The first is the number of Ducks, Duckiebots, then cones")
 
     def create_save_dir(self, dir_path):
         given_path = Path(dir_path)
@@ -34,7 +38,8 @@ class ImageSaver():
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
             user_in = str(input())
-            if user_in == ' ':
+            if ' ' in user_in:
+                self.num_items = user_in
                 self.save_flag = True
             rate.sleep()
 
@@ -47,7 +52,11 @@ class ImageSaver():
             self.save_image(cv_image)
 
     def save_image(self, image):
-        file_name = f"{self.save_dir_path}/{datetime.now().strftime('%d-%m-%Y_%H-%M')}-IM{self.image_counter}.jpg"
+        if self.use_for_dataset:
+            self.num_items = self.num_items.split(" ")
+            file_name = f"{self.save_dir_path}/{self.num_items[0]}DK_{self.num_items[1]}RB_{self.num_items[0]}CN_{datetime.now().strftime('%H-%M')}.jpg"
+        else:
+            file_name = f"{self.save_dir_path}/{datetime.now().strftime('%d-%m-%Y_%H-%M')}-IM{self.image_counter}.jpg"
         self.image_counter += 1
         cv2.imwrite(file_name, image)
         rospy.loginfo("Saved image succcessfully")
